3028e158447d7ba24aa3d32fdacd77fc94f3f475
KamilS <ksasin3@gmail.com>
2023-05-11 15:41:10 +0200

    Little change in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,10 +3,12 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className='bg-[#00bfff] fixed flex flex-row justify-between w-screen'>
+    <nav className='bg-[#00bfff] fixed flex flex-row justify-between w-screen text-lg'>
         <div className='px-5 py-2'>
             {/* Left side nav links */}
             <Link href={'/'}>Strona główna</Link>
+            <Link href={'/'} className='ml-9'>Katalog ryb</Link>
+            <Link href={'/'} className='ml-9'>Akcesoria</Link>
         </div>
         <div className='px-5 py-2'>
             {/* Right side nav links */}
