dca3d2f071f19401cfbe96a51a89341f2debbd35
KamilS <ksasin3@gmail.com>
2023-06-15 05:09:34 +0200

    Change padding in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,9 +5,9 @@ export default function Navbar() {
   return (
       <nav className="fixed z-10 flex items-center justify-between p-4 bg-blue-600 text-lg w-screen drop-shadow-[0_5px_5px_rgba(0,0,0,0.2)]">
           <div className="flex items-center">
-              <Link href="/" className="px-2">Strona Główna</Link>
-              <Link href="/katalog_ryb" className="px-2">Katalog Ryb</Link>
-              <Link href="/akcesoria" className="px-2">Akcesoria</Link>
+              <Link href="/" className="px-4">Strona Główna</Link>
+              <Link href="/katalog_ryb" className="px-4">Katalog Ryb</Link>
+              <Link href="/akcesoria" className="px-4">Akcesoria</Link>
           </div>
           <a href="/login" className="px-4">Zaloguj</a>
       </nav>
