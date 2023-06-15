0a61153e672818380a858ecc3f436f2e62ac03bc
KamilS <ksasin3@gmail.com>
2023-06-15 05:04:21 +0200

    Add login page and delete unused files

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import React, {useState} from 'react'
+import Image from 'next/image';
+import rybka from '@/public/niebieska_rybka.png'
+
+export default function Login() {
+    const [input, setInput] = useState({
+        email: "",
+        password: "",
+    })
+
+    // @ts-ignore
+    const onInputChange = (e) => {
+        const {name, value} = e.target
+        setInput({
+            ...input,
+            [name]: value
+        })
+    }
+
+    return (
+        <div className='flex flex-col min-h-screen items-center justify-center'>
+            <div className="p-10 flex flex-col items-center rounded-2xl bg-white">
+                <Image src={rybka} alt='Rybka logo' width={300} height={300}/>
+                <input type="text" placeholder='Email' name="email" value={input.email}
+                       onChange={onInputChange}
+                       className='border-b-2 border-b-gray-400 pl-1 my-1
+                       h-8 text-black focus:outline-none'/>
+                <input type="password" placeholder='Password' name="password" value={input.password}
+                       onChange={onInputChange}
+                       className='border-b-2 border-b-gray-400 pl-1 my-1
+                       h-8 text-black focus:outline-none'/>
+                <button onClick={() => {}}
+                        className='w-fit my-1 mt-2 py-2 px-3 rounded-lg bg-blue-600
+                        hover:bg-lightBlue hover:border-transparent
+                        transition-all duration-300'>
+                    Login
+                </button>
+            </div>
+        </div>
+    )
+}
